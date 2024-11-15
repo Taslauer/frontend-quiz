4a40ffa37147a89375f980a9ae12ad9eab496953
Taslauer <99174711+Taslauer@users.noreply.github.com>
2024-11-15 09:59:38 +0100

    Haslauer

---
+++ src/lib/fonts/components/Listitem.svelte
@@ -0,0 +1,19 @@
+<script>
+	let { icon, title, index, focusAction, correctAnswer, inncorrectlySelected, disabled } = $props();
+	let ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+</script>
+
+<button
+	class="alert flex focus:outline focus:outline-primary {correctAnswer
+		? 'outline outline-success'
+		: ''} {inncorrectlySelected ? 'outline outline-error' : ''}"
+	onfocus={focusAction}
+	{disabled}
+>
+	{#if icon}
+		<img src={icon} alt={title + '-Icon'} />
+	{:else}
+		<span> {ABC[index]}</span>
+	{/if}
+	<span>{title} - {correctAnswer}</span>
+</button>
---
+++ src/routes/[quiz]/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+	import { page } from '$app/stores';
+	import Listitem from '$lib/fonts/components/Listitem.svelte';
+	import { goto } from '$app/navigation';
+	let { data } = $props();
+	let currentQuestion = $state(0);
+	let selectedAnswer = $state('');
+	let score = $state(0);
+	let submitted = $state(false);
+
+	function handleSubmit() {
+		submitted = true;
+		data.questions[currentQuestion].selectedAnswer = selectedAnswer;
+		if (selectedAnswer === data.questions[currentQuestion].answer) {
+			score++;
+		}
+	}
+
+	function nextQuestion() {
+		submitted = false;
+		currentQuestion++;
+		selectedAnswer = '';
+	}
+
+	function navigateToMenu() {
+		goto('/');
+	}
+</script>
+
+{#if currentQuestion < data.questions.length}
+	{#each data.questions as question, index (question.question)}
+		{#if index === currentQuestion}
+			<p class="my-3">Question {index + 1} of {data.questions.length}</p>
+			<progress class="progress progress-primary w-56" value={currentQuestion + 1} max="10"
+			></progress>
+			<h1 class="my-3">{question.question}</h1>
+			{#each question.options as option, index (option)}
+				<div class="my-3">
+					{#key submitted}
+						<Listitem
+							title={option}
+							{index}
+							correctAnswer={question.selectedAnswer && question.answer === option}
+							inncorrectlySelected={question.selectedAnswer === option &&
+								question.answer !== option}
+							focusAction={() => (selectedAnswer = option)}
+							disabled={submitted}
+						></Listitem>
+					{/key}
+				</div>
+			{/each}
+		{/if}
+	{/each}
+
+	{#if !submitted}
+		<button class="btn btn-primary" onclick={handleSubmit} disabled={!selectedAnswer}>Submit</button
+		>
+	{:else}
+		<button class="btn btn-primary" onclick={nextQuestion}>Next Question</button>
+	{/if}
+{:else}
+	<h2>Game Over!</h2>
+	<p>You scored {score} out of {data.questions.length} points!</p>
+	<br />
+	<button class="btn btn-primary" onclick={window.location.reload()}> Play again!</button>
+	<br /><br />
+	<button class="btn btn-primary" onclick={navigateToMenu}>Menu</button>
+{/if}
